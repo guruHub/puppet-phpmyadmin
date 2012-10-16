172e7abb3ab3bab93458ef0b47b9d619196c14ea
Cédric Michaux <cedric@he8us.be>
2012-10-16 20:34:31 +0200

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,21 @@
+class phpmyadmin{
+
+	package { "phpmyadmin":
+		ensure => installed,
+		require => [Service["apache2"], Package["php5"]]
+	}
+
+	exec{ "link phpmyadmin config":
+		command => "ln -s /etc/phpmyadmin/apache.conf /etc/apache2/conf.d/phpmyadmin.conf",
+		unless => "/bin/readlink -e /etc/apache2/conf.d/phpmyadmin.conf",
+		require => Package["phpmyadmin"],
+		notify => Service["apache2"],
+	}
+
+	file{"phpmyadmin config":
+		path => "/usr/share/php/config.inc.php",
+		content => template("phpmyadmin/config.inc.php.erb")
+		require => Package["phpmyadmin"]
+	}
+
+}
---
+++ templates/config.inc.php.erb.php
@@ -0,0 +1,8 @@
+<?php
+
+	$i=0;
+	$i++;
+	$cfg['Servers'][$i]['user']          = '<%= dbuser %>';
+	$cfg['Servers'][$i]['password']      = '<%= dbpass %>'; // use here your password
+	$cfg['Servers'][$i]['auth_type']     = 'config';
+?>
