bff60996f28b3a17e1246b820e04f60cb75d606e
Cédric Michaux <cedric@he8us.be>
2012-10-17 04:41:45 +0200

    Use tarball (most recent version)

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,21 +1,56 @@
-class phpmyadmin{
+class phpmyadmin(
+	$dbuser = "puppet",
+	$dbpass = "puppet_user",
+	$dbname = "puppet",
+	$installpath => "/usr/share/phpmyadmin"
+){
 
-	package { "phpmyadmin":
-		ensure => installed,
-		require => [Service["apache2"], Package["php5"]]
-	}
 
-	exec{ "link phpmyadmin config":
-		command => "ln -s /etc/phpmyadmin/apache.conf /etc/apache2/conf.d/phpmyadmin.conf",
-		unless => "/bin/readlink -e /etc/apache2/conf.d/phpmyadmin.conf",
-		require => Package["phpmyadmin"],
+	file { '/tmp/phpMyAdmin-3.5.3.tar.gz':
+		ensure => file,
+		owner => root,
+		group => root,
+		source => 'puppet:///phpmyadmin/phpMyAdmin-3.5.3.tar.gz',
+    }
+
+    exec { 'untar phpmyadmin':
+		command => '/bin/tar -xvzf /tmp/phpMyAdmin-3.5.3.tar.gz',
+		creates => '/tmp/phpMyAdmin-3.5.3-all-languages',
+		cwd => "/tmp",
+		group => root,
+		user => root,
+
+		require => File['/tmp/phpMyAdmin-3.5.3.tar.gz'],
+    }
+
+	exec { '/bin/mv /tmp/phpMyAdmin-3.5.3-all-languages ${installpath}':
+		group => root,
+		user => root,
+		require => Exec['untar phpmyadmin'],
+    }
+
+	file {'${installpath}':
+			ensure => directory,
+			group => 'www-data',
+			recurse => true,
+			owner => "www-data",
+			require => Exec['untar phpmyadmin'],
+    }
+
+	file{ "create phpmyadmin apache config":
+		content => template("phpmyadmin/phpmyadmin.conf.erb"),
+		path => "/etc/apache2/conf.d/phpmyadmin.conf"
+		require => File["${installpath}"],
 		notify => Service["apache2"],
 	}
 
 	file{"phpmyadmin config":
-		path => "/usr/share/php/config.inc.php",
+		path => "/usr/share/phpmyadmin/config.inc.php",
 		content => template("phpmyadmin/config.inc.php.erb"),
-		require => Package["phpmyadmin"]
+		require => Package["phpmyadmin"],
+		owner => "www-data",
+		group => "www-data",
+		mode => 644
 	}
 
 }
