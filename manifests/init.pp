cecb361ad434b5415d39500d1b0bfc5f0b305e9e
gargrag <correo@gaston.com.uy>
2012-11-12 18:10:16 -0200

    changed installation order

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,8 @@
 class phpmyadmin(
-	$dbuser = "puppet",
-	$dbpass = "puppet_user",
-	$dbname = "puppet",
-	$installpath = "/usr/share/"
+  $dbuser = "puppet",
+  $dbpass = "puppet_user",
+  $dbname = "puppet",
+  $installpath = "/usr/share/"
 ){
 
   $pma_version = '3.5.3'
@@ -11,34 +11,26 @@ class phpmyadmin(
   exec { 'download latests pma':
     command => "/usr/bin/wget -o /dev/null -O /tmp/phpmyadmin.latest.tar.gz http://sourceforge.net/projects/phpmyadmin/files/phpMyAdmin/${pma_version}/phpMyAdmin-${pma_version}-english.tar.gz/download",
     creates => "/tmp/phpmyadmin.latest.tar.gz"
-  }
-
-  exec { 'untar phpmyadmin':
-		command => '/bin/tar -xzvf /tmp/phpmyadmin.latest.tar.gz',
-		creates => "/tmp/phpMyAdmin-${pma_version}-english",
-		cwd     => "/tmp",
-		group   => root,
-		user    => root,
-		require => Exec['download latests pma']
-  }
-
-	exec { 'Move to the install path':
-		command => "/bin/mv /tmp/phpMyAdmin-${pma_version}-english ${installpath}",
-		group => root,
-		user => root,
-		onlyif => "/usr/bin/test ! -d ${installpath}",
-		require => [ Exec['untar phpmyadmin'], File[$installpath] ]
-  }
-
-	file {$installpath:
+  } -> exec { 'untar phpmyadmin':
+    command => '/bin/tar -xzvf /tmp/phpmyadmin.latest.tar.gz',
+    creates => "${installpath}/pphpMyAdmin-${pma_version}-english",
+    cwd     => "/tmp",
+    group   => root,
+    user    => root,
+    require => Exec['download latests pma']
+  } -> file {$installpath:
     ensure => directory,
     recurse => true,
     group => 'www-data',
     owner => "www-data",
     mode => 644,
-	}
-
-  file { "${installpath}/phpmyadmin":
+  } -> exec { 'Move to the install path':
+    command => "/bin/mv /tmp/phpMyAdmin-${pma_version}-english ${installpath}",
+    group => root,
+    user => root,
+    onlyif => "/usr/bin/test ! -d ${installpath}",
+    require => [ Exec['untar phpmyadmin'], File[$installpath] ]
+  } -> file { "${installpath}/phpmyadmin":
     ensure  => lynk,
     target  => "${installpath}/phpMyAdmin-${pma_version}-english",
     require => Exec['Move to the install path']
